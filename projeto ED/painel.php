c9904cc1f0280121f6b176bcf4832278a0db7e98
Yan Magalhães <yanstrangerb@gmail.com>
2025-05-22 21:40:35 -0300

    add banco de dados

--- projeto ED/painel.php
+++ projeto ED/painel.php
@@ -1,3 +1,9 @@
+<?php
+
+require_once("includes/conectarBD.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -52,6 +58,25 @@
               
         </div>
     </nav>
+
+    <?php
+        while ($arrayFilme = mysqli_fetch_array($sqlFilme)) {
+    ?>
+            <tr>
+                <td width="10%" height="25"><b>
+                        <font face="Arial" size="2"><?php echo $arrayFilme['idFil']; ?></font>
+                </td>
+                <td width="20%" height="25"><b>
+                        <font face="Arial" size="2"><?php echo $arrayFilme['nomeFil']; ?></font>
+                </td>
+                <td width="10%" height="25"><b>
+                        <font face="Arial" size="2"><?php echo $arrayFilme['sinopseFil']; ?></font>
+                </td>
+            </tr>
+
+    <?php
+        }
+    ?>
 
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
