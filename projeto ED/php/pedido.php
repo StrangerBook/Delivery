249a1582013bf0226cb25a20b387d98eef95e7c1
Yan Magalhães <yanstrangerb@gmail.com>
2025-05-26 19:51:31 -0300

    update

--- projeto ED/php/pedido.php
+++ projeto ED/php/pedido.php
@@ -132,9 +132,63 @@ require_once("../includes/conectarBD.php");
     }
 }
 ?>
+        </div>
     </div>
-</div>
+
+
+    <div class="barra-lateral">
+        <div class="container text-center d-flex justify-content-center" style="font-size: 20px;">
+            <p><b>Meu Carrinho</b></p>
+        </div>
+
+        <!-- Itens do carrinho aqui -->
+        <div class="items-carrinho">
+            <div class="container text-center d-flex justify-content-center" id="item-carrinho">
+                <div class="row row-cols">
+                    <div class="col align-items-center">
+                        <img src="../imagens/pizza1.jpeg" class="img-thumbnail">
+                    </div>
+                </div>
+            </div>
+
+            <div class="container text-center d-flex justify-content-center" id="item-carrinho">
+                <div class="row row-cols">
+                    <div class="col align-items-center">
+                        item
+                    </div>
+                    <div class="col align-items-center">
+                        preço
+                    </div>
+                </div>
+            </div>
+            <div class="container text-center d-flex justify-content-center" id="item-carrinho">
+                <div class="row row-cols">
+                    <div class="col align-items-center">
+                        <form action="" method="POST">
+                            <input type="hidden" name="" value="">
+                            <button type="submit">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                                </svg>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Mensagem de carrinho vazio aqui -->
+        <div class="container text-center d-flex justify-content-center" style="font-size: 15px;">
+            <p>Carrinho vazio</p>
+
+        </div>
+    </div>
+
+
+
 <div style="margin-top: 50px"></div>
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 
