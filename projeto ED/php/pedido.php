0d899137d01273c0481847343427d6956c88c569
Yan Magalhães <yanstrangerb@gmail.com>
2025-05-25 22:16:24 -0300

    painel e pedido

--- projeto ED/php/pedido.php
+++ projeto ED/php/pedido.php
@@ -12,7 +12,7 @@ require_once("../includes/conectarBD.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="../style/estilo.css">
-    <title>Document</title>
+    <title>Pedido</title>
 </head>
 
 <body>
@@ -134,6 +134,7 @@ require_once("../includes/conectarBD.php");
 ?>
     </div>
 </div>
+
 <div style="margin-top: 50px"></div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
